b89b930d7dbeb1499b313146457bf13f4729c948
dor-bynet <123317116+dor-bynet@users.noreply.github.com>
2023-02-27 16:56:25 +0200

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -3,7 +3,8 @@ import re
 
 def binomial(n):
     for i in range(n+1):
-        print('1', end="")
+        coef = '1'
+        print(re.sub(r'(1)', r'\033[90m\1\033[0m', coef), end="")
         for j in range(1, i+1):
             coef = str(i * (i-j+1) // j)
             print(' (' + re.sub(r'(\d)', r'\033[34m\1\033[0m', coef) + ')', end="")
