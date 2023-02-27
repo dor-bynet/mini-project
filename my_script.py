bdea3ddd6ab281dc8f6e23c271cbdf73d415b3dd
dor-bynet <123317116+dor-bynet@users.noreply.github.com>
2023-02-27 17:11:26 +0200

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -2,12 +2,12 @@
 def newton_binomial(n): 
     for i in range(0, n): 
         for j in range(0, n-i-1): 
-            print(" ", end=" ")
+            print("  ", end=" ")
         for j in range(0, i+1): 
             if(j == 0 or i == j): 
                 print("\033[91m" + str(1) + "\033[0m", end=" ") 
             else: 
-                print("\033[94m" + str(int(binomialCoeff(i, j))) + "\033[0m", end=" ")
+                print("\033[94m" + '{:2d}'.format(int(binomialCoeff(i, j))) + "\033[0m", end=" ")
         print("") 
      
 def binomialCoeff(n, k): 
