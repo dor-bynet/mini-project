bdf70476dd20ad15183667d5e47aa65f9f423ae9
dor-bynet <123317116+dor-bynet@users.noreply.github.com>
2023-02-27 17:09:01 +0200

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
-import sys
+def newton_binomial(n): 
+    for i in range(0, n): 
+        for j in range(0, n-i-1): 
+            print(" ", end=" ")
+        for j in range(0, i+1): 
+            if(j == 0 or i == j): 
+                print("\033[91m" + str(1) + "\033[0m", end=" ") 
+            else: 
+                print("\033[94m" + str(int(binomialCoeff(i, j))) + "\033[0m", end=" ")
+        print("") 
+     
+def binomialCoeff(n, k): 
+    res = 1
+    if (k > n - k): 
+        k = n - k 
+    for i in range(0 , k): 
+        res = res * (n - i) 
+        res = res // (i + 1) 
+    return res 
 
-# Set the text color to red
-sys.stdout.write("\x1b[31m")
-
-# Print the 1 digits
-for i in range(1, 11):
-    for j in range(1, i+1):
-        if (j == 1 or j == i):
-            # Set the text color to red
-            sys.stdout.write("\x1b[31m")
-            print(j, end=" ")
-        else:
-            # Set the text color to blue
-            sys.stdout.write("\x1b[34m")
-            print(j, end=" ")
-    print()
-
-# Set the text color to default
-sys.stdout.write("\x1b[0m")
+n = int(input("Enter Number of rows: ")) 
+newton_binomial(n)
