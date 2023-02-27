a43c78b52b63f872eea55a1d68077c1913d42bed
dor-bynet <123317116+dor-bynet@users.noreply.github.com>
2023-02-27 16:57:56 +0200

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -2,9 +2,14 @@
 import re
 
 def binomial(n):
-    for i in range(n+1):
+    for i in range(n + 1):
+        # Print leading spaces
+        for j in range(n - i):
+            print(' ', end='')
+        # Print first number
         coef = '1'
         print(re.sub(r'(1)', r'\033[90m\1\033[0m', coef), end="")
+        # Print rest of row
         for j in range(1, i+1):
             coef = str(i * (i-j+1) // j)
             print(' (' + re.sub(r'(\d)', r'\033[34m\1\033[0m', coef) + ')', end="")
