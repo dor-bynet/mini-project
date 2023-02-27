fc53541359f9faf8b0c759ce5db1e7c8367a8c02
dor-bynet <123317116+dor-bynet@users.noreply.github.com>
2023-02-27 17:36:05 +0200

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
-def newton_binomial(n): 
-    for i in range(0, n): 
-        for j in range(0, n-i-1): 
-            print("   ", end=" ")
-        for j in range(0, i+1): 
-            if(j == 0 or i == j): 
-                print("\033[91m" + str(1) + "\033[0m", end=" ") 
-            else: 
-                print("\033[94m" + str(int(binomialCoeff(i, j))) + "\033[0m", end=" ")
-        print("") 
-     
-def binomialCoeff(n, k): 
-    res = 1
-    if (k > n - k): 
-        k = n - k 
-    for i in range(0 , k): 
-        res = res * (n - i) 
-        res = res // (i + 1) 
-    return res 
+import numpy as np
+import matplotlib.pyplot as plt
 
-newton_binomial(10)
+def newton_binomial(n):
+    coeffs = np.zeros((n+1, n+1))
+    coeffs[0][0] = 1
+
+    for i in range(1, n+1):
+        coeffs[i][0] = 1
+        for j in range(1, i+1):
+            coeffs[i][j] = coeffs[i-1][j-1] + coeffs[i-1][j]
+
+    max_coeff = np.max(coeffs)
+    num_digits = len(str(max_coeff))
+
+    for i in range(n+1):
+        row_str = ""
+        for j in range(i+1):
+            coeff_str = str(int(coeffs[i][j])).rjust(num_digits)
+            row_str += coeff_str + " "
+        padding = " " * ((n-i) * (num_digits+1) // 2)
+        print(padding + row_str)
+
+    plt.imshow(coeffs, cmap='Blues', interpolation='nearest')
+    plt.title(f"Newton binomial coefficients for n={n}")
+    plt.show()
+
+newton_binomial(15)
